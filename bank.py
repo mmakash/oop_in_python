88b5da449f74e72e4e01575354d30bc2ef91b0a6
akash <musaddekurakash@gmail.com>
2024-12-08 04:42:29 +0600

    changemethod overloaded

--- bank.py
+++ bank.py
@@ -6,3 +6,17 @@ class Account:
         self.accNo = accNo
         self.password = password
         self.type = type
+
+        Account.account.append(self)
+
+    def changeInfo(self,name):
+        self.name = name
+        print(f"name change to{name}")
+    #overload the changeInfo method
+    def changeInfo(self,newName,newsPass):
+        self.newName = newName
+        self.newPass = newsPass
+        print(f"name change to {newName},{newsPass}")
+
+myacc = Account("ratin",1234,1234,"saving")
+myacc.changeInfo("gorib",123)
