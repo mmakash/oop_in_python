76a4b56939f276d6b7f5d9a4e6406d40a3d7be8d
akash <musaddekurakash@gmail.com>
2024-12-08 03:39:34 +0600

    bank project added

---
+++ bank.py
@@ -0,0 +1,8 @@
+class Account:
+    account = []
+
+    def __init__(self,name,accNo,password,type):
+        self.name = name
+        self.accNo = accNo
+        self.password = password
+        self.type = type
